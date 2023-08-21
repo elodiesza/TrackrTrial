ac2a6e3bbca89ae07bd5f9d68997fc0a04f60be9
ginger <elodie.sza@gmail.com>
2023-08-22 08:35:19 +0900

    creation of my own database from script

--- Analytics/ScaleHabit.js
+++ Analytics/ScaleHabit.js
@@ -5,6 +5,8 @@ function ScaleHabit (scales,habits,direction) {
         const habitname=habits[0].name;
         const scalename=scales[0].name;
 
+// Calculate limit with 80% outcome
+
         const minvalue = Math.min(...scales.filter(c=>c.value!==null).map(c=>c.value));
         const maxvalue = Math.max(...scales.filter(c=>c.value!==null).map(c=>c.value));
 
@@ -24,42 +26,85 @@ function ScaleHabit (scales,habits,direction) {
             results.push({'limit':i,'infTrueCount':parseInt((trueCount*100/totalInfCount).toFixed(2)),'supFalseCount':parseInt((falseCount*100/totalSupCount).toFixed(2))});
         }
 
-        let infresults =results;
-        let supresults =results;
         // for True inf to value
+        const infresults =results;
         const infCleanedResults = () => {
             for (var i=0;i<infresults.length;i++){
                 infresults[i]=(infresults[i].infTrueCount<80 || infresults[i].supFalseCount<80) ? {'limit':i,'infTrueCount':null,'supFalseCount':null} : infresults[i];
             }
             return infresults;
         }
-        const inflimitArray = infresults.map(c=>c.infTrueCount+c.supFalseCount);
+        const inflimitArray = infCleanedResults().map(c=>c.infTrueCount+c.supFalseCount);
         const inflimitValue = Math.max(...inflimitArray)==undefined? null : Math.max(...inflimitArray);
         const inflimitIndex = inflimitValue==null? null : inflimitArray.indexOf(inflimitValue);
+        const infTruevalue = infCleanedResults().map(c=>c.infTrueCount)[inflimitIndex];
         const inflimit = (inflimitIndex==null||inflimitIndex==-1)? null : results[inflimitIndex].limit;
 
+
         // for True sup to value
+        const supresults =results;
         const supCleanedResults = () => {
             for (var i=0;i<supresults.length;i++){
                 supresults[i]=(supresults[i].infTrueCount>20 || supresults[i].supFalseCount>20) ? {'limit':i,'infTrueCount':null,'supFalseCount':null} : supresults[i];
             }
             return supresults;
         }
-
-        const suplimitArray = supresults.map(c=>c.supTrueCount+c.supFalseCount);
+        const suplimitArray = supCleanedResults().map(c=>c.supTrueCount+c.supFalseCount);
         const suplimitValue = Math.min(...suplimitArray)==undefined? null : Math.min(...suplimitArray);
         const suplimitIndex = suplimitValue==null? null : suplimitArray.indexOf(suplimitValue);
+        const supFalsevalue = supCleanedResults().map(c=>c.supFalseCount)[suplimitIndex];
         const suplimit = (suplimitIndex==null||suplimitIndex==-1)? null : results[suplimitIndex].limit;
+       
+        
+        const result = (inflimit!==null && inflimit!==0)?
+        'You are more likely to do '+habitname+' when '+scalename+' is under '+inflimit+'. ('+ infTruevalue+'% of the time.)' :
+        (suplimit!==null && suplimit!==0)?
+            'You are more likely to do '+habitname+' when '+scalename+' is over '+suplimit+'. ('+ supFalsevalue+'% of the time.)' :
+            'There is no direct correlation between '+habitname+' and '+scalename+'.';
 
 
 
-        const result = inflimit!==null?
-                            'You are more likely to do '+habitname+' when '+scalename+' is under '+inflimit+'. ('+ infresults.map(c=>c.infTrueCount)[inflimitIndex]+'% of the time.)' :
-                            suplimit!==null?
-                                'You are more likely to do '+habitname+' when '+scalename+' is over '+suplimit+'. ('+ supresults.map(c=>c.supTrueCount)[suplimitIndex]+'% of the time.)' :
-                                'There is no correlation between '+habitname+' and '+scalename+'.';
+//Raise or decrease after habit change
 
-        return {'result': result,'resultNumber':100};
+        var raiseTrueCount = null;
+        var raiseFalseEqualCount = null;
+        var decreaseTrueEqualCount = null;
+        var decreaseFalseCount = null;
+        var raiseFalseCount = null;
+        var decreaseTrueCount = null;
+        var raiseTrueEqualCount = null;
+        var decreaseFalseEqualCount = null;
+
+        for (var i=0; i<scales.length-1;i++){
+            scales[i+1].value!==null && scales[i]!==null && scales[i+1].value>scales[i].value ? (habits[i].state==true ? raiseTrueCount++ : raiseFalseEqualCount++) : undefined;
+            scales[i+1].value!==null && scales[i]!==null && scales[i+1].value<scales[i].value ? (habits[i].state==false? decreaseFalseCount++ : decreaseTrueEqualCount++) : undefined;
+            scales[i+1].value!==null && scales[i]!==null && scales[i+1].value>scales[i].value ? (habits[i].state==false? raiseFalseCount++ : raiseTrueEqualCount++) : undefined;
+            scales[i+1].value!==null && scales[i]!==null && scales[i+1].value<scales[i].value ? (habits[i].state==true? decreaseTrueCount++ : decreaseFalseEqualCount++) : undefined;
+        };
+
+        const raiseTrueCountResult = (raiseTrueCount*100/(raiseTrueCount+raiseFalseEqualCount)).toFixed(2);
+        const decreaseFalseCountResult = (decreaseFalseCount*100/(decreaseFalseCount+decreaseTrueEqualCount)).toFixed(2);
+        const raiseFalseCountResult = (raiseFalseCount*100/(raiseFalseCount+raiseTrueEqualCount)).toFixed(2);
+        const decreaseTrueCountResult = (decreaseTrueCount*100/(decreaseTrueCount+decreaseFalseEqualCount)).toFixed(2);
+
+
+        //Here only direction==false is correct. For direction == true, need to see for scale i-1 and habit i.
+        const result2 = raiseTrueCountResult>80 && decreaseFalseCountResult>80 ? 
+                            (direction==true ?
+                                'You are likely to do ' +habitname +' when '+scalename +' increases ('+ raiseTrueCountResult+'%)':
+                                scalename +' is likely to increase when you do '+habitname+' ('+ raiseTrueCountResult+'%)'  
+                            ): 
+                            raiseFalseCountResult >80 && decreaseTrueCountResult >80 ?
+                                (direction==true ?
+                                    'You are likely to do ' +habitname +' when '+scalename +' decreases ('+ raiseFalseCountResult+'%)':
+                                    scalename +" is likely to increase when you don't do "+habitname+' ('+ raiseFalseCountResult+'%)' 
+                                ):
+                                'There is no raise/decrease correlation between '+habitname+' and '+scalename+'.';
+        
+        
+
+
+        return {'result': result+ ' '+ result2,'resultNumber':(inflimit!==null && inflimit!==0)?infTruevalue:(suplimit!==null && suplimit!==0)?supFalsevalue:0};
     }
     else{
         return {'result':'no result : undefined indicators','resultNumber':0};
